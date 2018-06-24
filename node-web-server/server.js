eb05b9f34bbb8a57617b1e7f32879c71bf6fe9d7
mohit <mjain8156@gmail.com>
2018-06-25 00:31:50 +0530

    Add middle-ware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine','hbs');
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    console.log(`${now}: ${req.method} ${req.url}`);
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log',log + '\n',(err) => {
+        if(err){
+            console.log('Unable to append gto server.log');
+        }
+    })
+    next();
+});
+// app.use((req,res,next) => {
+//     res.render('maintenance.hbs');
+// });
 app.get('/',(req , res) => {
     // res.send({
     //     name: 'Mohit',
